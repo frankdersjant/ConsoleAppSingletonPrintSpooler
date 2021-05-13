d2a53a397c4019adfab8a303765ed71377117d1e
frank <f.dersjant@inholland.nl>
2021-05-13 09:42:41 +0200

    added comments

--- ConsoleAppSingleSpooler/PrintSpooler.cs
+++ ConsoleAppSingleSpooler/PrintSpooler.cs
@@ -1,11 +1,15 @@
-﻿namespace ConsoleAppSingleSpooler
+﻿namespace ConsoleAppSingletonPrintSpooler
 {
     public sealed class PrintSpooler
     {
+        //Static: always only ONE instance no matter how many instances of classes 
+
         private static PrintSpooler instance = null;
 
         private PrintSpooler() { }
 
+        //null-coalescing assignment operator ??=
+        //assigns the value of its right-hand operand to its left-hand operand only if the left-hand operand evaluates to null.
         public static PrintSpooler Instance => instance ??= new PrintSpooler();
     }
 }
--- ConsoleAppSingleSpooler/Program.cs
+++ ConsoleAppSingleSpooler/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppSingletonPrintSpooler;
+using System;
 
 namespace ConsoleAppSingleSpooler
 {
