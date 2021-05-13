fad7275709a79d5b6a2177d85f6f2b6a96286d3d
frank <f.dersjant@inholland.nl>
2021-05-13 08:54:12 +0200

    Add project files.

---
+++ ConsoleAppSingleSpooler/PrintSpooler.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppSingleSpooler
+{
+    public sealed class PrintSpooler
+    {
+        private static PrintSpooler instance = null;
+
+        private PrintSpooler() { }
+
+        public static PrintSpooler Instance => instance ??= new PrintSpooler();
+    }
+}
---
+++ ConsoleAppSingleSpooler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppSingleSpooler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Print spooler is a software service that manages the printing process.
+            //The spooler accepts print jobs from computers and makes sure that printer resources are available.
+            //It also schedules the order in which jobs are sent to the print queue for printing.
+            //In the early days of personal computers, you had to wait until a document printed before you could do anything else.
+
+
+            PrintSpooler printSpooler1 = PrintSpooler.Instance;
+            PrintSpooler printSpooler2 = PrintSpooler.Instance;
+
+            if (printSpooler1 == printSpooler2)
+            {
+                Console.WriteLine("Both singletons contain the same the same reference");
+            }
+        }
+    }
+}
